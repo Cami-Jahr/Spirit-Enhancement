30653503b653b5a2865495e6bdd0a411985257d7
Cami-Jahr <joachimjahr@hotmail.com>
2020-07-08 22:26:22 +0200

    Updates

---
+++ precache-manifest.a80a7a3267c00b9fb1343c6b26538c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ef06504254d2b40b1d1749082ba852",
+    "url": "/Spirit-Enhancement/index.html"
+  },
+  {
+    "revision": "74ec4e63822c36271f86",
+    "url": "/Spirit-Enhancement/static/css/main.8f452c19.chunk.css"
+  },
+  {
+    "revision": "860588bfe0258ccc4cc4",
+    "url": "/Spirit-Enhancement/static/js/2.d87290f8.chunk.js"
+  },
+  {
+    "revision": "27852730bc28016d92fba720b01c9ae1",
+    "url": "/Spirit-Enhancement/static/js/2.d87290f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ec4e63822c36271f86",
+    "url": "/Spirit-Enhancement/static/js/main.5ded916b.chunk.js"
+  },
+  {
+    "revision": "b3fa5d12501f38c7401a",
+    "url": "/Spirit-Enhancement/static/js/runtime-main.7132912e.js"
+  }
+]);
