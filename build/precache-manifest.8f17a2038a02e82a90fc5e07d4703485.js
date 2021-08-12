92083d48947f36cc2edd1e1cf0ed6159d32ac188
Camilla <camilla.jahr@hotmail.com>
2021-08-12 23:38:14 +0200

    Updated build

---
+++ build/precache-manifest.8f17a2038a02e82a90fc5e07d4703485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d416d70beeebe38bcf06ac9380aee45a",
+    "url": "/Spirit-Enhancement/index.html"
+  },
+  {
+    "revision": "7543bf35ff5e11a8d472",
+    "url": "/Spirit-Enhancement/static/css/main.c9f21e3f.chunk.css"
+  },
+  {
+    "revision": "e445f5be2260dfc6f522",
+    "url": "/Spirit-Enhancement/static/js/2.971ba967.chunk.js"
+  },
+  {
+    "revision": "27852730bc28016d92fba720b01c9ae1",
+    "url": "/Spirit-Enhancement/static/js/2.971ba967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7543bf35ff5e11a8d472",
+    "url": "/Spirit-Enhancement/static/js/main.63889b64.chunk.js"
+  },
+  {
+    "revision": "b3fa5d12501f38c7401a",
+    "url": "/Spirit-Enhancement/static/js/runtime-main.7132912e.js"
+  }
+]);
